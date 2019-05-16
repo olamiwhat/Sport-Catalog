a74c6e1d9bd09a1f1ee3e7f51bc172fac4e90f61
Ola Adedoyin <19616857+olamiwhat@users.noreply.github.com>
2019-05-16 00:16:14 -0400

    Improved styling

--- database_setup.py
+++ database_setup.py
@@ -1,7 +1,7 @@
 import sys
 from sqlalchemy import Column, ForeignKey, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import relationship
+from sqlalchemy.orm import relationship, backref
 from sqlalchemy import create_engine
 
 
