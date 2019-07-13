6455a4f9dfa7815488560526b2437859d798f875
Ola Adedoyin <19616857+olamiwhat@users.noreply.github.com>
2019-07-13 19:01:08 -0400

    make file PEP8 compliant

--- lotsofitem.py
+++ lotsofitem.py
@@ -8,14 +8,15 @@ DBSession = sessionmaker(bind=engine)
 session = DBSession()
 
 
-#dummy user
+# dummy user
 User1 = User(name="John Doe", email="johndoe@mywebsite.com",
-             picture='https://cdn.pixabay.com/photo/2015/03/04/22/35/head-659651__340.png')
+             picture="https://cdn.pixabay.com/"
+             "photo/2015/03/04/22/35/head-659651__340.png")
 session.add(User1)
 session.commit()
 
 
-#items for Soccer
+# items for Soccer
 category1 = Category(user_id=1, name="Soccer")
 
 session.add(category1)
@@ -57,30 +58,31 @@ categoryItem5 = CategoryItem(user_id=1, name="Shin Guard",
 session.add(categoryItem5)
 session.commit()
 
-#items for Tennis
+# items for Tennis
 category2 = Category(user_id=1, name="Tennis")
 
 session.add(category2)
 session.commit()
 
 categoryItem1 = CategoryItem(user_id=1, name="Racket",
-                             description="A light bat using for striking the ball",
+                             description="A light bat used for"
+                             " striking the ball",
                              category=category2)
 
 session.add(categoryItem1)
 session.commit()
 
 categoryItem2 = CategoryItem(user_id=1, name="Overgrip",
-                             description="A thin, soft, sometimes padded"
-                             " layer wrapped around the tennis racket's handle",
+                             description="A thin, soft, sometimes padded "
+                             "layer wrapped around the tennis racket's handle",
                              category=category2)
 
 session.add(categoryItem2)
 session.commit()
 
 categoryItem3 = CategoryItem(user_id=1, name="Tennis Balls",
-                             description="A hollow ball made of rubber with"
-                             " a fuzzy covering of woven Dacron, nylon or wool",
+                             description="A hollow ball made of rubber with "
+                             "a fuzzy covering of woven Dacron, nylon or wool",
                              category=category2)
 
 session.add(categoryItem3)
@@ -89,7 +91,8 @@ session.commit()
 categoryItem4 = CategoryItem(user_id=1, name="Tennis Shoes",
                              description="Tennis shoes have non-marking soles,"
                              " special support for the demands of aggresive "
-                             "tennis movement, and comfort for the heavy lateral movement",
+                             "tennis movement, and comfort for the heavy "
+                             "lateral movement",
                              category=category2)
 
 session.add(categoryItem4)
